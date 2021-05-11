3533ab42fd368cd12ea61ed7d4dfd9d1ef915f38
bp-ooya <bp-ooya@kdl.co.jp>
2021-05-11 11:13:26 +0900

    テスト用コード

--- js/main.js
+++ js/main.js
@@ -2,34 +2,50 @@ miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'Sticker to shapes',
+        title: 'Reset',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
         onClick: async () => {
-          // Get selected widgets
-          let selectedWidgets = await miro.board.selection.get()
+
+          // 確認メッセージ表示
+          let confirm = confirm("ボードを初期化します。よろしいですか？");
+
+          if(confirm){
+
+            // 全Stickerオブジェクトの取得
+            let allStickers = await miro.board.widgets.get({type: 'sticker'})
+
+            // 初期化位置の取得
+            let frame1 = await miro.board.widgets.get({title: 'Frame 1'})
+
+            let x = frame1.x + 20;
+            let y = frame1.y + 80;
+
+            let irow = 0;
+            let icol = 0;
+
+            // StickerオブジェクトをFrame1上に移動
+            for(sticker in allStickers){
+              sticker.x = x;
+              sticker.y = y;
+
+              if(irow / 2 != 0){
+                y+=sticker.height + 20;
+              }
+
+              if(icol == 1){
+                x+=sticker.width + 20;
+                icol = 0;
+              }else{
+                x = frame1.x + 20;
+              }
+            }
+          }
 
           // Filter stickers from selected widgets
-          let stickers = selectedWidgets.filter((widget) => widget.type === 'STICKER')
-
-          // Delete selected stickers
-          await miro.board.widgets.deleteById(stickers.map((sticker) => sticker.id))
-
-          // Create shapes from selected stickers
-          await miro.board.widgets.create(
-            stickers.map((sticker) => ({
-              type: 'shape',
-              text: sticker.text,
-              x: sticker.x,
-              y: sticker.y,
-              width: sticker.bounds.width,
-              height: sticker.bounds.height,
-            })),
-          )
-
           // Show success message
-          miro.showNotification('Stickers has been converted')
+          miro.showNotification('ボードを初期化しました。')
         },
       },
     },
