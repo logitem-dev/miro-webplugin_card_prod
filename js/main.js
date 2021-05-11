1b230d82c6dcf6b0c498224a21a346daa3ad0d7a
bp-ooya <bp-ooya@kdl.co.jp>
2021-05-11 11:20:37 +0900

    画面クリアテスト

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
@@ -11,37 +13,37 @@ miro.onReady(() => {
           // 確認メッセージ表示
           let confirm = confirm("ボードを初期化します。よろしいですか？");
 
-          if(confirm){
+          // if(confirm){
 
-            // 全Stickerオブジェクトの取得
-            let allStickers = await miro.board.widgets.get({type: 'sticker'})
+          //   // 全Stickerオブジェクトの取得
+          //   let allStickers = await miro.board.widgets.get({type: 'sticker'})
 
-            // 初期化位置の取得
-            let frame1 = await miro.board.widgets.get({title: 'Frame 1'})
+          //   // 初期化位置の取得
+          //   let frame1 = await miro.board.widgets.get({title: 'Frame 1'})
 
-            let x = frame1.x + 20;
-            let y = frame1.y + 80;
+          //   let x = frame1.x + 20;
+          //   let y = frame1.y + 80;
 
-            let irow = 0;
-            let icol = 0;
+          //   let irow = 0;
+          //   let icol = 0;
 
-            // StickerオブジェクトをFrame1上に移動
-            for(sticker in allStickers){
-              sticker.x = x;
-              sticker.y = y;
+          //   // StickerオブジェクトをFrame1上に移動
+          //   for(sticker in allStickers){
+          //     sticker.x = x;
+          //     sticker.y = y;
 
-              if(irow / 2 != 0){
-                y+=sticker.height + 20;
-              }
+          //     if(irow / 2 != 0){
+          //       y+=sticker.height + 20;
+          //     }
 
-              if(icol == 1){
-                x+=sticker.width + 20;
-                icol = 0;
-              }else{
-                x = frame1.x + 20;
-              }
-            }
-          }
+          //     if(icol == 1){
+          //       x+=sticker.width + 20;
+          //       icol = 0;
+          //     }else{
+          //       x = frame1.x + 20;
+          //     }
+          //   }
+          // }
 
           // Filter stickers from selected widgets
           // Show success message
