54399c6ba65e9fd37e6f6ca79541fc7c20225dc9
bp-ooya <bp-ooya@kdl.co.jp>
2021-05-27 10:50:57 +0900

    ファイルアップロード

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'Reset',
+        title: 'ÉJÅ[Éhìoò^',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
         onClick: async () => {
 
-			await miro.board.ui.openModal('createsticker.html', { width: 500, height: 750 });
+			await miro.board.ui.openModal('createsticker.html', { width: 500, height: 500 });
 			
 
         },
